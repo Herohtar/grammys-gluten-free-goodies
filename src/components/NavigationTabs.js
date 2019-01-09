382f283e8b0e39192cb72e136a6a7d5341f772c2
Herohtar <belac1186@gmail.com>
2019-01-09 00:20:42 -0600

    Clean the trailing slash from the location path

--- src/components/NavigationTabs.js
+++ src/components/NavigationTabs.js
@@ -6,10 +6,15 @@ import Tab from '@material-ui/core/Tab'
 
 import * as routes from '../constants/routes'
 
+// location.pathname can be either '/some/path' or '/some/path/', but both are actually
+// the same route, so to make location matching work for both cases we need to remove the
+// trailing slash from the path string (with the exception of the root path, '/')
+const cleanPath = path => path.replace(/\/$/, '') || '/'
+
 const NavigationTabs = React.memo(() => (
   <Location>
     {({ location }) => (
-      <Tabs value={location.pathname} centered component="nav">
+      <Tabs value={cleanPath(location.pathname)} centered component="nav">
         <Tab component={Link} value={routes.HOME} to={routes.HOME} label="Home" />
         <Tab component={Link} value={routes.PRODUCTS} to={routes.PRODUCTS} label="Products" />
         <Tab component={Link} value={routes.ORDER} to={routes.ORDER} label="Order Form" />
