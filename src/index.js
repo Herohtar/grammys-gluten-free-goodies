2a9ed52cdfcccb2f78bea1e02b0c007eecc04831
Herohtar <belac1186@gmail.com>
2019-05-08 19:14:13 -0500

    Updated index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { AppContainer } from 'react-hot-loader'
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 
 // Your top level component
@@ -13,16 +12,15 @@ export default App
 
 // Render your app
 if (typeof document !== 'undefined') {
-  const renderMethod = module.hot ? ReactDOM.render : ReactDOM.hydrate || ReactDOM.render
+  const target = document.getElementById('root')
+  const renderMethod = target.hasChildNodes() ? ReactDOM.hydrate : ReactDOM.render
   const muiTheme = createMuiTheme(theme)
   const render = Comp => {
     renderMethod(
-      <AppContainer>
-        <MuiThemeProvider theme={muiTheme}>
-          <Comp />
-        </MuiThemeProvider>
-      </AppContainer>,
-      document.getElementById('root'),
+      <MuiThemeProvider theme={muiTheme}>
+        <Comp />
+      </MuiThemeProvider>,
+      target,
       () => {
         const jssStyles = document.getElementById('jss-server-side')
         if (jssStyles && jssStyles.parentNode) {
@@ -35,7 +33,7 @@ if (typeof document !== 'undefined') {
   // Render!
   render(App)
 
-  if (module.hot) {
-    module.hot.accept('./App', () => render(require('./App').default))
+  if (module && module.hot) {
+    module.hot.accept('./App', () => render(App))
   }
 }
